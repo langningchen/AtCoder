d6fa2b3fa65ab85158ebd808ecab2beafa3510bb
Chen LangNing <jpxx_chenlangning@outlook.com>
2023-11-24 17:38:20 +0800

    Last commit

---
+++ Run.fish
@@ -0,0 +1,11 @@
+#!/bin/fish
+if test (count $argv) -ne 2
+    echo "The script must be called from the vscode task."
+    exit 1
+end
+set Username (cat ./Username)
+set Password (cat ./Password)
+set OJ (pwd | sed 's/.*\///g')
+cd ./OJTool
+./Run.sh
+./build/main -u $Username -p $Password -oj $OJ -o $argv[1] -pr $argv[2]
